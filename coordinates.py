396b92ce9a8367554f0afa7ba1691aeba67b9214
Andrew McManus <andrewmcmanusmusic@gmail.com>
2021-02-24 14:16:34 -0500

    [coordinates.py] function latlong_js

--- coordinates.py
+++ coordinates.py
@@ -1,29 +1,38 @@
 import ast
-# import numpy as np
+import numpy as np
 from openpyxl import load_workbook
 import pandas as pd
 
 workbook = load_workbook(filename="cta-ridership-avg.-weekday-bus-stop-boardings-in-october-2012.xlsx")
 sheet = workbook.active
 values = sheet.values
+
+# print(df)
+cols = next(values)
 values = list(values)
+
 # LOCATION = 8
 df = pd.DataFrame(values)
 # location column:
-location_list = list(df[8])
 
-locations = []
-for i in location_list:
-    if i == location_list[0]:
-        print(i)
-    else:
-        dict = ast.literal_eval(i)
-        locations.append(dict)
-# print(locations)
-coordinates = []
-for i in locations:
-    lat = float(i['latitude'])
-    long = float(i['longitude'])
-    coordinates.append([lat, long])
-    # long = float(i['latitude'])
-print(coordinates)
+# stop Id column:
+# print(df[0])
+# ast.literal_eval(df[8])
+def latlong_js():
+    location_list = list(df[8])
+    locations = []
+    latlong = []
+    for i in location_list:
+        if i == location_list[0]:
+            print(i)
+        else:
+            dict = ast.literal_eval(i)
+            locations.append(dict)
+    for i in locations:
+        lat = float(i['latitude'])
+        long = float(i['longitude'])
+        latlong.append([lat, long])
+        coordinates = np.array(latlong)
+    return coordinates
+result = latlong_js()
+print(result)
